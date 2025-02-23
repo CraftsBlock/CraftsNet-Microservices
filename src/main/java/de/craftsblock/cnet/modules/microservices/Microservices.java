27101f9c9a436018116bd7c695558d43c93d282d
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-02-23 09:44:49 +0100

    Initial commit

---
+++ src/main/java/de/craftsblock/cnet/modules/microservices/Microservices.java
@@ -0,0 +1,9 @@
+package de.craftsblock.cnet.modules.microservices;
+
+import de.craftsblock.craftsnet.addon.Addon;
+import de.craftsblock.craftsnet.addon.meta.annotations.Meta;
+
+@Meta(name = "Microservices")
+public class Microservices extends Addon {
+
+}
